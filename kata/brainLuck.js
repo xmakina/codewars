802534073c49917a70de48265ef3c1b4cfe90050
Alex Addams <alex.addams@67bricks.com>
2019-01-07 09:56:11 +0000

    brainluck

--- kata/brainLuck.js
+++ kata/brainLuck.js
@@ -1,5 +1,5 @@
 module.exports = function brainLuck (code, input) {
-  let circuitBreaker = 10000
+  let circuitBreaker = 10000000
 
   let tokenMap = {
     ',': storeInput,
@@ -12,33 +12,41 @@ module.exports = function brainLuck (code, input) {
     '<': decrementDataPointer
   }
 
+  let tempPairs = []
+  let jumpPairs = []
   let tokens = code.split('')
-  let instructions = tokens.reduce((accumulator, value) => {
-    accumulator.push(tokenMap[value])
+  let instructions = tokens.reduce((accumulator, value, index) => {
+    let instruction = tokenMap[value]
+    if (instruction === jumpIfZero) {
+      tempPairs.push(accumulator.length)
+    }
+    if (instruction === jumpBackIfNonZero) {
+      jumpPairs.push({ start: tempPairs.pop(), end: accumulator.length })
+    }
+
+    accumulator.push(instruction)
     return accumulator
   }, [])
 
-  let state = makeState(input, instructions)
-  console.log(state)
+  let state = makeState(input, instructions, jumpPairs)
+  console.log(state.input, input)
   while (state.instructionPointer < instructions.length) {
-    let jump = state.getCurrentInstruction()(state)
-    if (!jump) {
-      state.instructionPointer++
-    }
+    state.getCurrentInstruction()(state)
+    state.instructionPointer++
 
     circuitBreaker--
     if (circuitBreaker === 0) {
       break
     }
   }
-  console.log(state)
+
   return state.output.reduce((accumulator, value) => {
     accumulator += String.fromCharCode(value)
     return accumulator
   }, '')
 }
 
-function makeState (input, instructions) {
+function makeState (input, instructions, jumpPairs) {
   return {
     dataPointer: 0,
     data: [],
@@ -46,8 +54,9 @@ function makeState (input, instructions) {
     input: input.split('').map(value => value.charCodeAt(0)).reverse(),
     instructionPointer: 0,
     instructions,
+    jumpPairs,
     getCurrentInstruction: function () { return this.instructions[this.instructionPointer] },
-    getCurrentData: function () { return this.data[this.dataPointer] },
+    getCurrentData: function () { return this.data[this.dataPointer] || 0 },
     updateData: function (value) { this.data[this.dataPointer] = value },
     incrementData: function () {
       if (this.data[this.dataPointer] === undefined) {
@@ -84,42 +93,18 @@ function decrementByte (state) {
 
 function jumpIfZero (state) {
   if (state.getCurrentData() === 0) {
-    findClosingJump(state)
-
-    return true
+    state.instructionPointer = state.jumpPairs.find(p => p.start === state.instructionPointer).end
   }
 }
 
 function jumpBackIfNonZero (state) {
   if (state.getCurrentData() !== 0) {
-    findOpeningJump(state)
-
-    return true
+    state.instructionPointer = state.jumpPairs.find(p => p.end === state.instructionPointer).start
   }
 }
 
 function output (state) {
   state.output.push(state.getCurrentData())
-}
-
-function findClosingJump (state) {
-  while (state.getCurrentInstruction() !== jumpBackIfNonZero) {
-    state.instructionPointer++
-    if (state.getCurrentInstruction() === jumpIfZero) {
-      findClosingJump(state)
-      state.instructionPointer++
-    }
-  }
-}
-
-function findOpeningJump (state) {
-  while (state.getCurrentInstruction() !== jumpIfZero) {
-    state.instructionPointer--
-    if (state.getCurrentInstruction() === jumpBackIfNonZero) {
-      findOpeningJump(state)
-      state.instructionPointer--
-    }
-  }
 }
 
 function incrementDataPointer (state) {
--- kata/brainLuck.spec.js
+++ kata/brainLuck.spec.js
@@ -2,17 +2,18 @@ const brainLuck = require('./brainLuck')
 const Test = require('../framework/Test')
 
 Test.describe('BrainLuck', function () {
-  Test.it('can echo until byte(255) encountred', () => {
-    Test.assertEquals(brainLuck(',+[-.,+]', 'Codewars' + String.fromCharCode(255)), 'Codewars')
-  })
-  Test.it('can echo until byte(0) encountred', () => {
-    Test.assertEquals(brainLuck(',[.[-],]', 'Codewars' + String.fromCharCode(0)), 'Codewars')
-  })
-  Test.it('multiply two numbers', () => {
-    Test.assertEquals(brainLuck(',>,<[>[->+>+<<]>>[-<<+>>]<<<-]>>.', String.fromCharCode(1, 0)), String.fromCharCode(0))
-  })
+  // Test.it('can echo until byte(255) encountred', () => {
+  //   Test.assertEquals(brainLuck(',+[-.,+]', 'Codewars' + String.fromCharCode(255)), 'Codewars')
+  // })
+  // Test.it('can echo until byte(0) encountred', () => {
+  //   Test.assertEquals(brainLuck(',[.[-],]', 'Codewars' + String.fromCharCode(0)), 'Codewars')
+  // })
+  // Test.it('multiply two numbers', () => {
+  //   Test.assertEquals(brainLuck(',>,<[>[->+>+<<]>>[-<<+>>]<<<-]>>.', String.fromCharCode(1, 0)), String.fromCharCode(0))
+  // })
   Test.it('a big program', () => {
-    Test.assertEquals(brainLuck(',>+>>>>++++++++++++++++++++++++++++++++++++++++++++>++++++++++++++++++++++++++++++++<<<<<<[>[>>>>>>+>+<<<<<<<-]>>>>>>>[<<<<<<<+>>>>>>>-]<[>++++++++++[-<-[>>+>+<<<-]>>>[<<<+>>>-]+<[>[-]<[-]]>[<<[>>>+<<<-]>>[-]]<<]>>>[>>+>+<<<-]>>>[<<<+>>>-]+<[>[-]<[-]]>[<<+>>[-]]<<<<<<<]>>>>>[++++++++++++++++++++++++++++++++++++++++++++++++.[-]]++++++++++<[->-<]>++++++++++++++++++++++++++++++++++++++++++++++++.[-]<<<<<<<<<<<<[>>>+>+<<<<-]>>>>[<<<<+>>>>-]<-[>>.>.<<<[-]]<<[>>+>+<<<-]>>>[<<<+>>>-]<<[<+>-]>[<+>-]<<<-]', '\n'),
-      '1, 1, 2, 3, 5, 8, 13, 21, 34, 55')
+    Test.assertEquals(brainLuck(
+      ',>+>>>>++++++++++++++++++++++++++++++++++++++++++++>++++++++++++++++++++++++++++++++<<<<<<[>[>>>>>>+>+<<<<<<<-]>>>>>>>[<<<<<<<+>>>>>>>-]<[>++++++++++[-<-[>>+>+<<<-]>>>[<<<+>>>-]+<[>[-]<[-]]>[<<[>>>+<<<-]>>[-]]<<]>>>[>>+>+<<<-]>>>[<<<+>>>-]+<[>[-]<[-]]>[<<+>>[-]]<<<<<<<]>>>>>[++++++++++++++++++++++++++++++++++++++++++++++++.[-]]++++++++++<[->-<]>++++++++++++++++++++++++++++++++++++++++++++++++.[-]<<<<<<<<<<<<[>>>+>+<<<<-]>>>>[<<<<+>>>>-]<-[>>.>.<<<[-]]<<[>>+>+<<<-]>>>[<<<+>>>-]<<[<+>-]>[<+>-]<<<-]', '\n'),
+    '1, 1, 2, 3, 5, 8, 13, 21, 34, 55')
   })
 })
