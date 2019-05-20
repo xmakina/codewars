69ec5722a08dbb16ffd8903724a307002386edb7
Alex Addams <alex.addams@67bricks.com>
2019-05-20 08:47:43 +0100

    ES Lint

--- framework/Test.js
+++ framework/Test.js
@@ -1,10 +1,23 @@
-/* global it */
+/* global it, describe */
 const chai = require('chai')
 
 module.exports = {
+  itAssertSimilar: function (actual, expected, msg, options) {
+    return it(msg || `${actual} should be similar to ${expected}`, () => {
+      return this.assertSimilar(actual, expected, msg, options)
+    })
+  },
   assertSimilar: function (actual, expected, msg, options) {
-    return it(`${actual} should be similar to ${expected}`, () => {
-      return chai.expect(actual).to.equal(expected)
+    return chai.expect(actual).to.deep.equal(expected)
+  },
+  describe,
+  it,
+  assertEquals: function (actual, expected, msg, options) {
+    return chai.expect(actual).to.equal(expected)
+  },
+  itAssertEquals: function (actual, expected, msg, options) {
+    return it(msg || '', () => {
+      this.assertEquals(actual, expected, msg, options)
     })
   }
 }
