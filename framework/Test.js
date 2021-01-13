dc361c4ceaefa0be1b2dfb6b805e249d1e2c3b16
Alex Addams <github@xmakina.co.uk>
2021-01-13 09:28:17 +0000

    tidy up test files to always create output

--- framework/Test.js
+++ framework/Test.js
@@ -1,23 +1,43 @@
 /* global it, describe */
 const chai = require('chai')
 
-module.exports = {
-  itAssertSimilar: function (actual, expected, msg, options) {
-    return it(msg || `${actual} should be similar to ${expected}`, () => {
-      return this.assertSimilar(actual, expected, msg, options)
-    })
-  },
-  assertSimilar: function (actual, expected, msg, options) {
-    return chai.expect(actual).to.deep.equal(expected)
-  },
-  describe,
-  it,
-  assertEquals: function (actual, expected, msg, options) {
-    return chai.expect(actual).to.equal(expected)
-  },
-  itAssertEquals: function (actual, expected, msg, options) {
-    return it(msg || '', () => {
-      this.assertEquals(actual, expected, msg, options)
-    })
+const assertEquals = function (actual, expected, hasMessage = false) {
+  const test = chai.expect(actual).to.equal(expected)
+
+  if (hasMessage) {
+    return test
   }
+
+  return it(`${actual} should be equal to ${expected}`, () => test)
+}
+
+const itAssertEquals = function (actual, expected, msg, options) {
+  return it(msg || `${actual} should be equal to ${expected}`, () => {
+    this.assertEquals(actual, expected, true)
+  })
+}
+
+const assertSimilar = function (actual, expected, hasMessage = false) {
+  const test = chai.expect(actual).to.deep.equal(expected)
+
+  if (hasMessage) {
+    return test
+  }
+
+  return it(`${actual} should be similar to ${expected}`, () => test)
+}
+
+const itAssertSimilar = function (actual, expected, msg, options) {
+  return it(msg || `${actual} should be similar to ${expected}`, () => {
+    return assertSimilar(actual, expected, true)
+  })
+}
+
+module.exports = {
+  assertSimilar,
+  itAssertSimilar,
+  assertEquals,
+  itAssertEquals,
+  describe,
+  it
 }
